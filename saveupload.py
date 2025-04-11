759c27a00adf615411be3e3417e4c2a14cc1e9f6
floris6 <osmbot333@gmail.com>
2025-04-12 01:08:50 +0200

    Update saveupload.py

--- saveupload.py
+++ saveupload.py
@@ -1,4 +1,5 @@
-# copy link to reddit downloader (DI
-# Put downloaded video on buffer
-# (custom chrome driver?)
-# https://www.fileconverto.com/video-aspect-ratio/ to convert if ratio is not 9:16
+# copy link to reddit downloader 
+# https://www.fileconverto.com/video-aspect-ratio/ to convert if ratio is not 9:16 (2e download...)
+
+# Put downloaded video on buffer --> (custom chrome driver?)
+# avoid double video?
